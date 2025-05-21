b099011b2e7eda698484905e984ebbdf35ad5b52
Thiru Sam <github@skandaapi.online>
2025-05-21 09:03:40 +0530

    v13

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1492107124"
+    serviceWorkerVersion: "56251190"
   }
 });
